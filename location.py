7c0ce8b8f062f9a41a263892c3822ac31a8a1fc9
joshtch <howejosh@gmail.com>
2016-12-18 20:12:18 -0500

    Fix test case

--- location.py
+++ location.py
@@ -117,4 +117,4 @@ if __name__ == "__main__":
 	loc1 = Location("Washington")
 	loc2 = Location("Chicago")
 	print "Delay from " + loc1.city + " to " + loc2.city + " is" \
-	, str(loc1.propagationDelayFrom(loc2)) + " ms"
+	+ str(loc1.propagationDelayFrom(loc2)) + " ms"
